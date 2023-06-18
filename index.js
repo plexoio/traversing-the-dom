52cfba1da1053c86ce14151edbb426aef5b4a1c8
Frank Reinaldo Arellano Andrade <plexonft@gmail.com>
2023-06-18 23:01:45 +0000

    finish methods for traversing Mt.DOM

---
+++ index.js
@@ -0,0 +1,31 @@
+/** Methods to navigate/traverse the DOM
+ * 
+ * parent() // upper
+ * children('') // downer, specify if many, same scope
+ * next() // same scope
+ * prev() // same scope
+ * toggleClass('') // on & off class
+ * 
+ * first('') // of the same kind "''"
+ * last('') // of the same kind "''"
+ * siblings('') // of the same scope "''", could be specified one of many
+ * filter('//matching_key') // of the same kind "''" remove those not matching & apply criteria .chaining if matching
+    $('p').filter('.intro').addClass('highlight'); // all 'p' with '.intro' class
+ */
+
+
+$('#reset').click(function(){
+	$(this).siblings().children().removeClass('red').removeClass('yellow').removeClass('green').removeClass('large-font');
+});
+
+$('#card-btn-1').click(function(){
+	$(this).siblings('span').addClass('yellow');
+});
+
+$('#card-btn-2').click(function(){
+    $(this).siblings('#card-par-1').addClass('red');
+});
+
+$('#card-btn-3').click(function(){
+	$(this).siblings().addClass('large-font');
+});
